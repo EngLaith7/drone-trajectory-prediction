1d02de3ce66ec5254a18e4a03e755a000ff91922
aymandahmali2002@gmail.com <aymandahmali2002>
2025-09-12 20:08:31 +0300

    edit Visualization and eda

--- src/data/try_func_of_Vis.py
+++ src/data/try_func_of_Vis.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import Visualization as vz
 
-# تحميل البيانات
+# Load data
 #make sure that you copied the imu_data.csv file to the data folder
 data_path = Path("data") / "imu_data.csv"
 df = vz.load_data(data_path)
 
-# استدعاء الدوال حسب الحاجة
+# Call visualization functions according to the need
 vz.plot_accelerometer_distribution(df)
 vz.plot_gyroscope_distribution(df)
 vz.plot_magnetometer_distribution(df)
@@ -15,4 +15,4 @@ vz.plot_gyroscope_timeseries(df)
 vz.plot_3d_trajectory(df)
 vz.plot_orientation(df)
 vz.plot_correlation_heatmap(df)
-    
+vz.plot_trajectory_partial(df, 1000)  
